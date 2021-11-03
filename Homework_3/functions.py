35f9063853a06893185691e5dad737de78a715eb
andreeabmanea <andreeamanea66@gmail.com>
2021-11-03 14:19:33 +0200

    [H3-5]: minimax almost done

--- Homework_3/functions.py
+++ Homework_3/functions.py
@@ -1,12 +1,13 @@
 import random
+import itertools
 
 
-def get_initial_color_list(n: int, m: int, k: int) -> list:
+def get_initial_color_list(n: int, m: int) -> list:
     color_list = []
     for i in range(0, n):
         for j in range(0, m):
             color_list.append(i)
-    return random.sample(color_list, k)
+    return color_list
 
 
 def is_final_state(state: list, initial_state: list, round: int, n: int):
@@ -19,7 +20,7 @@ def is_final_state(state: list, initial_state: list, round: int, n: int):
             return False
 
 
-def choose_random_color_list(k: int, color_list: list):
+def choose_goal_list(k: int, color_list: list):
     return random.sample(color_list, k)
 
 
@@ -35,14 +36,14 @@ def play_game(initial_state: list, n: int, m: int):
     available_colors = [color for color in range(m)]
     while True:
         turn += 1
-        print(f"---Round {turn}---")
+        print(f"---Round {turn}/{2*n}---")
         print(
             f"Input a combination of {len(initial_state)} balls from {available_colors}"
         )
         string_input = input()
         round_list = string_input.split(" ")
         round_list = [int(ball) for ball in round_list]
-        print(compare_sequences(round_list, initial_state))
+        print(f"You have guessed {compare_sequences(round_list, initial_state)} balls!")
         if is_final_state(round_list, initial_state, turn, n) == "A":
             print("Player A won!")
             break
@@ -52,4 +53,49 @@ def play_game(initial_state: list, n: int, m: int):
                 break
 
 
-play_game(get_initial_color_list(3, 3, 4), 3, 3)
+# play_game(get_initial_color_list(3, 3, 4), 3, 3)
+
+
+def minimax_alphabeta(
+    initial_state, depth: int, state: list, alpha, beta, turn, n, m, k, count
+):
+    balls = get_initial_color_list(n, m)
+    print(balls)
+    print(initial_state)
+    if is_final_state(state, initial_state, turn, n) != False or depth == 0:
+        return compare_sequences(state, initial_state)
+    else:
+        print(list(itertools.permutations(balls, k)))
+        for choice in list(itertools.permutations(balls, k)):
+            choice = list(choice)
+            score = minimax_alphabeta(
+                initial_state,
+                depth - 1,
+                choice,
+                alpha,
+                beta,
+                turn + 1,
+                n,
+                m,
+                k,
+                count + 1,
+            )
+            if score >= beta:
+                print(count)
+                return beta
+            if score > alpha:
+                alpha = score
+
+
+minimax_alphabeta(
+    choose_goal_list(2, get_initial_color_list(2, 3)),
+    2,
+    [],
+    -1000,
+    -1000,
+    0,
+    2,
+    3,
+    2,
+    0,
+)
